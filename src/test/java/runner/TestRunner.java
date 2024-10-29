ca2a8537cf5bdf006a2e57b113217623ad45734d
HakanTetik <fildisili88@gmail.com>
2024-10-29 19:26:10 +0100

    jenkins dosyasında değişiklik yapıldı

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -2,18 +2,15 @@ package runner;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
-
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {"pretty",
                 "json:target/cucumber.json",
-                "utils.formatter.PrettyReports:target/cucumber-pretty-reports"},
+                "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"},
         features = "src/test/resources/features",
         glue = "stepdefinitions",
         tags = "",
         dryRun = false
 )
 public class TestRunner {
-
-
 }
